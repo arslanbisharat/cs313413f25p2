64fa4404558bb27bcbe10992a09149250f0a7acd
Arslan Bisharat <thearslan380@gmail.com>
2025-09-19 21:22:54 -0500

    Add performance testing implementation and timing measurements

--- src/test/java/cs271/lab/list/TestPerformance.java
+++ src/test/java/cs271/lab/list/TestPerformance.java
@@ -7,20 +7,15 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestPerformance {
+public class TestPerformance{
 
   // TODO run test and record running times for SIZE = 10, 100, 1000, 10000, ...
   // (choose in conjunction with REPS below up to an upper limit where the clock
   // running time is in the tens of seconds)
   // TODO Question: What conclusions can you draw about the performance of LinkedList vs. ArrayList when
   // comparing their running times for AddRemove vs. Access? Record those running times in README.txt!
-  // TODO (optional) refactor to DRY
-  // which of the two lists performs better as the size increases?
-  private final int SIZE = 10;
-
-  // TODO choose this value in such a way that you can observe an actual effect
-  // for increasing problem sizes
-  private final int REPS = 1000000;
+  private int SIZE = 10;
+  private int REPS = 1000000;
 
   private List<Integer> arrayList;
 
@@ -73,4 +68,71 @@ public class TestPerformance {
       sum += arrayList.get(r % SIZE);
     }
   }
+    private long timeArrayListAddRemove() {
+        long startTime = System.currentTimeMillis();
+        for (var r = 0; r < REPS; r++) {
+            arrayList.add(0, 77);
+            arrayList.remove(0);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    private long timeLinkedListAddRemove() {
+        long startTime = System.currentTimeMillis();
+        for (var r = 0; r < REPS; r++) {
+            linkedList.add(0, 77);
+            linkedList.remove(0);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    private long timeArrayListAccess() {
+        long startTime = System.currentTimeMillis();
+        var sum = 0L;
+        for (var r = 0; r < REPS; r++) {
+            sum += arrayList.get(r % SIZE);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+
+    private long timeLinkedListAccess() {
+        long startTime = System.currentTimeMillis();
+        var sum = 0L;
+        for (var r = 0; r < REPS; r++) {
+            sum += linkedList.get(r % SIZE);
+        }
+        return System.currentTimeMillis() - startTime;
+    }
+    public static void main(String[] args) {
+        TestPerformance test = new TestPerformance();
+
+        int[] sizes = {10, 100, 1000, 10000};
+        int[] reps = {1000000, 100000, 10000, 1000};
+
+        System.out.println("Performance Test Results:");
+        System.out.println("SIZE\tREPS\tArrayList AddRemove\tLinkedList AddRemove\tArrayList Access\tLinkedList Access");
+
+        for (int i = 0; i < sizes.length; i++) {
+            test.SIZE = sizes[i];
+            test.REPS = reps[i];
+
+            try {
+                test.setUp();
+
+                long arrayAddRemoveTime = test.timeArrayListAddRemove();
+                test.setUp();
+                long linkedAddRemoveTime = test.timeLinkedListAddRemove();
+                test.setUp();
+                long arrayAccessTime = test.timeArrayListAccess();
+                test.setUp();
+                long linkedAccessTime = test.timeLinkedListAccess();
+
+                System.out.printf("%d\t%d\t%d ms\t\t%d ms\t\t%d ms\t\t%d ms%n",
+                        sizes[i], reps[i], arrayAddRemoveTime, linkedAddRemoveTime, arrayAccessTime, linkedAccessTime);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
